4fb7bce1121314c85232bf6e672bb0999b956c12
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-02-14 15:12:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int price) {
+        double answer = (price < 100000) ? price : 
+            (price < 300000) ? price*0.95 :
+            (price < 500000) ? price*0.9 : price*0.8;
+        return (int) answer;
+    }
+}
