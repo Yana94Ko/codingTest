74d36f602296dc88c9578d905a5e6e0cf272aa2c
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-17 15:12:09 +0900

    [level 0] Title: 배열의 평균값, Time: 1.71 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] numbers) {
-        double sum = 0;
-        for(int i : numbers){
-            sum += i;
-        }
-        double answer = sum / numbers.length;
-        return answer;
+        // double sum = 0;
+        // for(int i : numbers){
+        //     sum += i;
+        // }
+        // double answer = sum / numbers.length;
+        // return answer;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
