61cb653d2d52e3258e9c69c42e84a2e05c845fa8
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 03:18:08 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.82 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] array, int n) {
-        //1. for문을 이용한 풀이
-        int answer = 0;
-        for(int num : array){
-            if(num == n){
-                answer++;
-            }
-        }
-        return answer;
+        // //1. for문을 이용한 풀이
+        // int answer = 0;
+        // for(int num : array){
+        //     if(num == n){
+        //         answer++;
+        //     }
+        // }
+        // return answer;
+        
+        //2. stream을 사용한 풀이
+        return (int) Arrays.stream(array).filter(num -> num ==n).count();
     }
 }
