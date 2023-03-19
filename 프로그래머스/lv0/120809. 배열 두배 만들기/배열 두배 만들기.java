1404eb8b27faf9796a877202908758e154da0027
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 01:58:18 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.07 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        //1. for문을 이용한 기본 풀이
-        int [] answer = new int [numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            answer[i] = numbers[i] * 2;
-        }
-        return answer;
+        // //1. for문을 이용한 기본 풀이
+        // int [] answer = new int [numbers.length];
+        // for(int i = 0; i < numbers.length; i++){
+        //     answer[i] = numbers[i] * 2;
+        // }
+        // return answer;
+        
+        //2. Stream을 이용한 해법
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
 }
