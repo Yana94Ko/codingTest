7eac4962fbc817419b2992b6d7ee68c153d61afb
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 02:50:33 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.15 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList <Integer> answer = new ArrayList<>();
-        for(int i = 0; i <= n; i++){
-            if(i % 2 == 1) answer.add(i);
-        }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        // //1. for문과 arrayList를 사용한 풀이
+        // ArrayList <Integer> answer = new ArrayList<>();
+        // for(int i = 0; i <= n; i++){
+        //     if(i % 2 == 1) answer.add(i);
+        // }
+        // return answer.stream().mapToInt(Integer::intValue).toArray();
+        
+        //2.stream을 이용한 풀이
+        return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 1).toArray();
     }
 }
