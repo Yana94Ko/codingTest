3256f3be6ad7c2f472310b53d75fadbca7ea442c
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 02:11:01 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.32 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
-        return Arrays.stream(array).sorted().toArray()[array.length / 2];
+        // //1. stream 을 이용한 해법
+        // return Arrays.stream(array).sorted().toArray()[array.length / 2];
+        
+        //2. Array의 sort를 사용한 해법
+        Arrays.sort(array);
+        return array[array.length / 2];
     }
 }
