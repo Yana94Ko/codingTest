7633bd4167f3cf2d69fb350d83f3b7c9aacd85e5
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-19 21:56:04 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 6.52 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,14 +1,20 @@
 class Solution {
     public String solution(String my_string, int n) {
-        // 1. for문을 이용한 기본 풀이
-        String answer = "";
-        String [] spells = my_string.split("");
-        for(int i = 0; i < spells.length; i++){
-            for(int j = 0; j < n; j++){
-                answer += spells[i];
-            }
-        }
-        return answer;
+        // // 1. for문을 이용한 기본 풀이
+        // String answer = "";
+        // String [] spells = my_string.split("");
+        // for(int i = 0; i < spells.length; i++){
+        //     for(int j = 0; j < n; j++){
+        //         answer += spells[i];
+        //     }
+        // }
+        // return answer;
         
+        //2. Sting builder을 이용한 풀이
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < my_string.length(); i++){
+            sb.append((my_string.charAt(i)+"").repeat(n));
+        }
+        return sb.toString();
     }
 }
