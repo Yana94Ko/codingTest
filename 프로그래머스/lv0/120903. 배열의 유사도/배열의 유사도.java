a0ad6062d1b8f9ac1bd92e49af17c9909e95315c
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 03:29:40 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,10 +1,21 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
+        // //1. compareTo() 사용
+        // int answer = 0;
+        // for(String str1 : s1){
+        //     for(String str2 : s2){
+        //         if (str1.compareTo(str2) == 0) {
+        //             answer++;
+        //         }
+        //     }
+        // }
+        // return answer;
+        
+        //2. equals() 사용
         int answer = 0;
-        //1. compareTo() 사용
         for(String str1 : s1){
             for(String str2 : s2){
-                if (str1.compareTo(str2) == 0) {
+                if (str1.equals(str2)) {
                     answer++;
                 }
             }
