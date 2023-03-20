1ba26f66d55a6403212685a65f006533b82bed4c
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 17:58:37 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.34 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -2,13 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] sides) {
-        //for문을 이용한 풀이
-        int max = sides[0], mid = sides[0], min = sides[0];
-        for(int side : sides){
-            max = (max < side) ? side : max;
-            mid = (mid < side) ? (side < max) ? side : mid : mid;
-            min = (min > side) ? side : min;
-        }
-        return (max - mid - min < 0 ) ? 1 : 2;
+        // //for문을 이용한 풀이
+        // int max = sides[0], mid = sides[0], min = sides[0];
+        // for(int side : sides){
+        //     max = (max < side) ? side : max;
+        //     mid = (mid < side) ? (side < max) ? side : mid : mid;
+        //     min = (min > side) ? side : min;
+        // }
+        // return (max - mid - min < 0 ) ? 1 : 2;
+        
+        //2. Arrays를 사용한 풀이
+        Arrays.sort(sides);
+        return (sides[2] - sides[1] - sides[0] < 0) ? 1 : 2;
     }
 }
