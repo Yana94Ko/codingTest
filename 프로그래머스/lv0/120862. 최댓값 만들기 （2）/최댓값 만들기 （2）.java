f479189eac296f6b5d6760325310ae2dd4d57ed0
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-21 00:57:35 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.48 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,14 +1,20 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        //1. 기본 풀이
-        int max = numbers[0]*numbers[1];
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = 0; j < numbers.length; j++){
-                if(i != j && max < numbers[i] * numbers[j]){
-                    max = numbers[i] * numbers[j];
-                }
-            }
-        }
-        return max;
+        // //1. 기본 풀이
+        // int max = numbers[0]*numbers[1];
+        // for(int i = 0; i < numbers.length; i++){
+        //     for(int j = 0; j < numbers.length; j++){
+        //         if(i != j && max < numbers[i] * numbers[j]){
+        //             max = numbers[i] * numbers[j];
+        //         }
+        //     }
+        // }
+        // return max;
+        
+        //2. sort를 사용한 풀이
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length-2] * numbers[numbers.length-1]);
     }
 }
