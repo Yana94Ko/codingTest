49dc9e0691c144c32082f9f8e596f98cd6703338
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-20 23:39:36 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int n, int t) {
-        //1. 기본 문법 풀이
-        int answer = n;
-        while(t != 0){
-            answer = answer * 2;
-            t -= 1;
-        }
-        return answer;
+        // //1. 기본 문법 풀이
+        // int answer = n;
+        // while(t != 0){
+        //     answer = answer * 2;
+        //     t -= 1;
+        // }
+        // return answer;
+        
+        // //2. Math.pow 활용
+        // return n * ((int) Math.pow(2, t));
+        
+        //3. 증가하는 비율이 2의 제곱인 상황에서 사용 할 수 있는 비트연산자
+        return n << t;
     }
 }
