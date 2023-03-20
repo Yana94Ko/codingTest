239339089f7f7438c02070edb57e0762eeda9fd5
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-21 00:04:33 +0900

    [level 0] Title: 주사위의 개수, Time: 6.67 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,9 +1,15 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] box, int n) {
-        //1. 기본 풀이
-        for(int i = 0; i < box.length; i++){
-            box[i] = box[i] / n;
-        }
-        return box[0] * box[1] * box[2];
+        // //1. 기본 풀이
+        // for(int i = 0; i < box.length; i++){
+        //     box[i] = box[i] / n;
+        // }
+        // return box[0] * box[1] * box[2];
+        
+        //2. Stream 활용 풀이
+        int [] answer = IntStream.range(0, box.length).map(i -> box[i] / n).toArray();
+        return answer[0] * answer[1] * answer[2];
     }
 }
