ac97e004cffa40a07c0144ace7c8854695ef9f9b
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-01-05 15:25:01 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.22 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,16 +1,22 @@
+class Solution {
+    public long solution(long n) {
+        double sqrtN = Math.sqrt(n);
+        return Math.floor(sqrtN) == sqrtN ? (long) Math.pow(sqrtN + 1, 2) : -1;
+    }
+}
 // import java.util.stream.LongStream;
 
 // class Solution {
 //     public long solution(long n) {
 //         return (LongStream
-//             .range(1, n/2)
+//             .range(1, (long) Math.sqrt(n)+1)
 //             .filter(v -> n % v ==0 && n/v == v)
 //             .count() != 0)
-//             ? (long) Math.pow( Math.sqrt(n)+1, 2) : -1;
+//             ? (long) Math.pow( Math.sqrt(n) + 1, 2) : -1;
 //     }
 // }
-class Solution {
-    public long solution(long n) {
-        return n % Math.sqrt(n) == 0 ? (long) Math.pow( Math.sqrt(n)+1,2) : -1;
-    }
-}
+// class Solution {
+//     public long solution(long n) {
+//         return n % Math.sqrt(n) == 0 ? (long) Math.pow( Math.sqrt(n)+1,2) : -1;
+//     }
+// }
