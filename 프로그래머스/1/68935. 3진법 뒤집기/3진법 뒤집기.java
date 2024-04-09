1c9bbdba1bcda112a336a4ebd407dcfc9ce5dc10
YanaKo <91898700+Yana94Ko@users.noreply.github.com>
2024-04-09 20:47:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toString(n,3));
+        
+        return Integer.parseInt(sb.reverse().toString(),3);
+    }
+}
