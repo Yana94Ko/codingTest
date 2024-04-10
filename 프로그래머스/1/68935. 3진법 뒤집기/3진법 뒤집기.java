589b2766ca4a2c27347666cd32e905d8e8965162
YanaKo <91898700+Yana94Ko@users.noreply.github.com>
2024-04-10 18:04:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,6 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         StringBuilder sb = new StringBuilder();
+        int result = 0,squareNumber = 0;
+        
+        while(n > 0){
+            sb.append(n%3);
+            n /= 3;
+        }
+        
+        String thernaryNumber = sb.reverse().toString();
+        while(sb.length()>0){
+            result += Math.pow(3,squareNumber) * Integer.parseInt(Character.toString(sb.charAt(0)));
+            sb.deleteCharAt(0);
+            squareNumber+=1;
+        }
+        return result;
+    }
+    public int solution2(int n) {
+        StringBuilder sb = new StringBuilder();
         sb.append(Integer.toString(n,3));
         
         return Integer.parseInt(sb.reverse().toString(),3);
