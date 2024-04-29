8464444f9aa72ef30325c82cf745c144839c05db
YanaKo <91898700+Yana94Ko@users.noreply.github.com>
2024-04-29 17:37:42 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.77 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,24 @@
+function solution(today, terms, privacies) {
+    var answer = [];
+    today = new Date(today);
+    var idx = 1;
+    for(let privacy of privacies){
+        var writeDate = new Date(privacy.split(" ")[0]);
+        let writeTerm = privacy.split(" ")[1];
+        var endDate = writeDate;
+        
+        for(let term of terms){
+            if(term.split(" ")[0] == writeTerm){
+                let whileMonth = parseInt(term.split(" ")[1]);
+                endDate = new Date(endDate.setMonth(endDate.getMonth() + whileMonth));
+                endDate = new Date(endDate.setDate(endDate.getDate()-1));
+            }
+        }
+        console.log(endDate, today)
+        if(endDate < today){
+            answer.push(idx);
+        }
+        idx ++;
+    }
+    return answer;
+}
